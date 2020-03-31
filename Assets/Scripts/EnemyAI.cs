d8e96e8675750b3aacd7ea3b12a6d783d5ef6638
ilpeppino <ilpeppino@gmail.com>
2020-03-31 13:56:53 +0200

    171 - imported standard assets (+ fix), created sandbox environment, player and enemy with navmesh

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform _target;
+    NavMeshAgent _enemyNav;
+
+
+    private void Awake()
+    {
+        _enemyNav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _enemyNav.SetDestination(_target.position);
+    }
+
+}
